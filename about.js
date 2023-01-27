792f8a18b65f169bcfa769b47dcf73f8cda5dc31
sudhan1256 <97861502+sudhan1256@users.noreply.github.com>
2023-01-27 15:43:16 +0530

    Add files via upload

---
+++ about.js
@@ -0,0 +1,9 @@
+function showMore() {
+    let moreInfo = document.getElementById("more-info");
+    if (moreInfo.style.display === "none") {
+      moreInfo.style.display = "block";
+    } else {
+      moreInfo.style.display = "none";
+    }
+  }
+  
